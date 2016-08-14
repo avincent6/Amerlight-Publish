1afdf8eb1a8459513129256743415813b5eaebcf
Audrey Vincent <vincent6@purdue.edu>
2016-08-14 11:51:27 -0500

    static files fuck them all

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,7 +139,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles/')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATIC_URL = '/static/'
 
@@ -148,8 +148,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-WHITENOISE_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'root')
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 COMPRESS_ENABLED = True;
 
 COMPRESS_PRECOMPILERS = (
