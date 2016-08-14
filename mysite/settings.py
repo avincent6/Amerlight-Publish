e66964cd36a78bdb6f540ee57dd51e96b06f360f
Audrey Vincent <vincent6@purdue.edu>
2016-08-14 12:35:19 -0500

    syntax error;

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 import dj_database_url
 from django.core.exceptions import ImproperlyConfigured
-import local_settings import *
+from local_settings import *
 
 def get_env_variable(var_name):
     """ Get the environment variable or return exception """
