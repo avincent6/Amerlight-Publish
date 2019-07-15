861b201c5b76e5f2962643c6513c9ac9c015031b
avincent6 <vincent6@purdue.edu>
2019-07-15 17:16:24 -0500

    update skey

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,7 +36,7 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env("SKEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
